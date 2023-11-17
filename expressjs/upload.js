3325cefbfda9b9338a15ab40964bd4f95f62574c
Intamixx <intamixx@users.noreply.github.com>
2023-11-17 16:33:25 +0000

    Update upload.js

--- expressjs/upload.js
+++ expressjs/upload.js
@@ -1,15 +1,142 @@
+'use strict'
+
+const http = require('http');
+//const axios = require('axios');
+
+/**
+ * Module dependencies.
+ */
+
+//var express = require('../../');
 const express = require('express');
+var path = require('path');
+var fs = require('fs');
+
+//var app = module.exports = express();
+const app = express();
+
+var options = {
+    key: fs.readFileSync('./ssl/privatekey.pem'),
+    cert: fs.readFileSync('./ssl/certificate.pem'),
+};
+var port = 9090;
+
+// Register ejs as .html. If we did
+// not call this, we would need to
+// name our views foo.ejs instead
+// of foo.html. The __express method
+// is simply a function that engines
+// use to hook into the Express view
+// system by default, so if we want
+// to change "foo.ejs" to "foo.html"
+// we simply pass _any_ function, in this
+// case `ejs.__express`.
+
+app.engine('.html', require('ejs').__express);
+
+// Optional since express defaults to CWD/views
+
+app.set('views', path.join(__dirname, 'views'));
+
+// Path to our public directory
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Without this you would need to
+// supply the extension to res.render()
+// ex: res.render('users.html').
+app.set('view engine', 'html');
+
+// Dummy users
+var users = [
+  { name: 'tobi', email: 'tobi@learnboost.com' },
+  { name: 'loki', email: 'loki@learnboost.com' },
+  { name: 'jane', email: 'jane@learnboost.com' }
+];
+
+app.get('/', function(req, res){
+  res.render('users', {
+    users: users,
+    title: "EJS example",
+    header: "Some users"
+  });
+});
+
+function gethttpaxios() {
+axios.get('http://localhost:8000/status/0ba3c83ce691d814ee31c3b944177d96-u8jgk')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+    return (response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+}
+
+
+function gethttp_api(callback) {
+
+console.log("-----------------");
+console.log(global.id);
+console.log("-----------------");
+http.get('http://localhost:8000/' + global.stype + '/' + global.id, res => {
+  let data = [];
+  const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
+  console.log('Status Code:', res.statusCode);
+  console.log('Date in Response header:', headerDate);
+
+  res.on('data', chunk => {
+    data.push(chunk);
+  });
+
+  res.on('end', () => {
+    console.log('Response ended: ');
+    var status = JSON.parse(Buffer.concat(data).toString());
+    //console.log(status);
+    callback(res.statusCode, status);
+  });
+}).on('error', err => {
+  console.log('Error: ', err.message);
+  callback(err.message, null);
+});
+
+}
+
+var callback = function(data) {
+  //if (err) throw err; // Check for the error and throw if it exists.
+  console.log('got data: '+data); // Otherwise proceed as usual.
+//  return ("TUTI");
+};
+
+var usingitnow = function(callback) {
+  //var myError = new Error('My custom error!');
+  //callback(myError, 'get it?'); // I send my error as the first argument.
+  //return ("TUTI");
+  var status = callback();
+  console.log("!!!!!!!!!!!")
+  console.log(status)
+  console.log("!!!!!!!!!!!")
+  //callback('2nd');
+  //var status = gethttp_status();
+  //var status = gethttpaxios();
+  //var status = testshite();
+  //return (status);
+};
+
+//const express = require('express');
 const multer = require('multer');
 const https = require('https');
-const app = express();
+//const app = express();
 var fs = require('fs');
 
 var maxFileSizeInBytes = 8000000;
 
-var options = {
-	    key: fs.readFileSync('./ssl/privatekey.pem'),
-	    cert: fs.readFileSync('./ssl/certificate.pem'),
-};
+//var options = {
+//	    key: fs.readFileSync('./ssl/privatekey.pem'),
+//	    cert: fs.readFileSync('./ssl/certificate.pem'),
+//};
 var port = 9090;
 
 const whitelist = [
@@ -198,8 +325,8 @@ app.post('/upload', function (req, res) {
     if (req.file.size > maxFileSizeInBytes) {
        return res.status(413).json({detail: "File upload size limit exceeded"});
     }
-    filepath = req.file['path'];
-    filename = req.file['originalname'];
+    var filepath = req.file['path'];
+    var filename = req.file['originalname'];
     console.log(filepath);
     console.log(filename);
     //console.log(req.body);
@@ -214,15 +341,33 @@ app.post('/upload', function (req, res) {
         //console.log("RETURN 1");
         console.log('after start ' + fast_api_response);
         console.log("STATUS is " + fast_api_response['http_status']);
-        console.log("BODY is " + fast_api_response['http_body']);
+        //console.log("BODY is " + fast_api_response['http_body']);
+	console.log("JSON is " + JSON.stringify(fast_api_response['http_body']));
         console.log(typeof(fast_api_response['http_status']));
+	var tokens = JSON.stringify(fast_api_response['http_body']).split(" ");
+	var id = tokens[tokens.length - 1];
+	id = id.replace('}', '');
+	id = id.replace('{', '');
+	id = id.replace('"', '');
+	console.log(id);
+
         if (!fast_api_response) {
             console.log("NOTHING");
         }
         //msg = "{ detail: " + fast_api_response['http_body'] + " }";
         switch (fast_api_response['http_status']) {
             case 200:
-                res.status(fast_api_response['http_status']).json(fast_api_response['http_body']);
+		// Extract the id from the response body
+                //res.status(fast_api_response['http_status']).render('status-wrapper', {
+                //    id: id,
+                //    status: JSON.stringify(fast_api_response['http_body']),
+                //    title: "Kueue Job Status",
+                //    header: "Some info about job status"
+                //  });
+                //res.status(fast_api_response['http_status']).json(fast_api_response['http_body']);
+		const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
+		await sleep(1000);
+		res.status(fast_api_response['http_status']).redirect("/status/" + id);
                 break;
             case 413:
                 res.status(fast_api_response['http_status']).json(fast_api_response['http_body']);
@@ -251,10 +396,135 @@ app.post('/upload', function (req, res) {
 })
 
 //app.listen(9090)
-var server = https.createServer(options, app).listen(port, function(){
-  console.log("Express server listening on port " + port);
-  });
+//var server = https.createServer(options, app).listen(port, function(){
+//  console.log("Express server listening on port " + port);
+//  });
 
 
 // Then we will set the storage 
 //const upload = multer({ storage: storage })
+
+
+app.get('/status/:id', (req, res) => {
+    global.id = req.params.id;
+    global.stype = "status";
+    console.log("!!!!!!!!");
+    console.log(id);
+    //var status = "";
+    //var data = "";
+    gethttp_api(function(err, status, id) {
+        if (err) console.log('error', err)//error handling
+	console.log(err);
+        console.log(status);
+  		res.status(err).render('status-wrapper', {
+		    users: users,
+		    id: global.id,
+		    status: JSON.stringify(status),
+		    title: "Kueue Job Status",
+		    header: "Some info about job status"
+		  });
+    	});
+});
+
+
+app.get('/render/status/:id', (req, res) => {
+    global.id = req.params.id;
+    global.stype = "status";
+    console.log("!!!!!!!!");
+    console.log(id);
+    //var status = "";
+    //var data = "";
+    gethttp_api(function(err, status, id) {
+        if (err) console.log('error', err)//error handling
+	console.log(err);
+        console.log(status);
+  		res.status(err).render('status', {
+		    users: users,
+		    id: global.id,
+		    status: JSON.stringify(status),
+		    title: "Kueue Job Status",
+		    header: "Some info about job status"
+		  });
+    	});
+});
+
+app.get('/api/status/:id', (req, res) => {
+    global.id = req.params.id;
+    global.stype = "status";
+    console.log("!!!!!!!!");
+    console.log(id);
+    //var status = "";
+    //var data = "";
+    gethttp_api(function(err, status, id) {
+        if (err) console.log('error', err)//error handling
+	console.log(err);
+        console.log(status);
+	        res.status(err).send(status);
+    	});
+});
+
+app.get('/result/:id', (req, res) => {
+    global.id = req.params.id;
+    global.stype = "result";
+    console.log("!!!!!!!!");
+    console.log(id);
+    //var status = "";
+    //var data = "";
+    gethttp_api(function(err, result, id) {
+        if (err) console.log('error', err)//error handling
+        console.log(result);
+  		res.status(err).render('result-wrapper', {
+		    users: users,
+		    id: global.id,
+		    result: JSON.stringify(result),
+		    title: "Kueue Job Result",
+		    header: "Some info about job result"
+		  });
+    	});
+});
+
+app.get('/render/result/:id', (req, res) => {
+    global.id = req.params.id;
+    global.stype = "result";
+    console.log("!!!!!!!!");
+    console.log(id);
+    //var status = "";
+    //var data = "";
+    gethttp_api(function(err, result, id) {
+        if (err) console.log('error', err)//error handling
+        console.log(result);
+  		res.status(err).render('result', {
+		    users: users,
+		    id: global.id,
+		    result: JSON.stringify(result),
+		    title: "Kueue Job Result",
+		    header: "Some info about job result"
+		  });
+    	});
+});
+
+app.get('/api/result/:id', (req, res) => {
+    global.id = req.params.id;
+    global.stype = "result";
+    console.log("!!!!!!!!");
+    console.log(id);
+    //var status = "";
+    //var data = "";
+    gethttp_api(function(err, result, id) {
+        if (err) console.log('error', err)//error handling
+        console.log(result);
+	        res.status(err).send(result);
+  		//res.status(err).render('result-api', {
+    	});
+});
+
+
+
+/* istanbul ignore next */
+//if (!module.parent) {
+//  app.listen(9090);
+//  console.log('Express started on port 9090');
+//}
+var server = https.createServer(options, app).listen(port, function(){
+  console.log("Express server listening on port " + port);
+});
