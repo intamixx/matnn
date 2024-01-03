a2591799c50034492304e4bdc0a2afa200afbc3b
Intamixx <intamixx@users.noreply.github.com>
2024-01-03 13:52:35 +0000

    Create setup.sh

---
+++ musicnn/setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+curl -s -o models/mood_acoustic-musicnn-msd-2.pb https://essentia.upf.edu/models/classifiers/mood_acoustic/mood_acoustic-musicnn-msd-2.pb
+     curl -s -o models/mood_acoustic-musicnn-mtt-2.pb https://essentia.upf.edu/models/classifiers/mood_acoustic/mood_acoustic-musicnn-mtt-2.pb
+     curl -s -o models/mood_acoustic-vggish-audioset-1.pb https://essentia.upf.edu/models/classifiers/mood_acoustic/mood_acoustic-vggish-audioset-1.pb
+
+     curl -s -o models/mood_aggressive-musicnn-msd-2.pb https://essentia.upf.edu/models/classifiers/mood_aggressive/mood_aggressive-musicnn-msd-2.pb
+     curl -s -o models/mood_aggressive-musicnn-mtt-2.pb https://essentia.upf.edu/models/classifiers/mood_aggressive/mood_aggressive-musicnn-mtt-2.pb
+     curl -s -o models/mood_aggressive-vggish-audioset-1.pb https://essentia.upf.edu/models/classifiers/mood_aggressive/mood_aggressive-vggish-audioset-1.pb
+
+     curl -s -o models/mood_electronic-musicnn-msd-2.pb https://essentia.upf.edu/models/classifiers/mood_electronic/mood_electronic-musicnn-msd-2.pb
+     curl -s -o models/mood_electronic-musicnn-mtt-2.pb https://essentia.upf.edu/models/classifiers/mood_electronic/mood_electronic-musicnn-mtt-2.pb
+     curl -s -o models/mood_electronic-vggish-audioset-1.pb https://essentia.upf.edu/models/classifiers/mood_electronic/mood_electronic-vggish-audioset-1.pb
+
+     curl -s -o models/mood_happy-musicnn-msd-2.pb https://essentia.upf.edu/models/classifiers/mood_happy/mood_happy-musicnn-msd-2.pb
+     curl -s -o models/mood_happy-musicnn-mtt-2.pb https://essentia.upf.edu/models/classifiers/mood_happy/mood_happy-musicnn-mtt-2.pb
+     curl -s -o models/mood_happy-vggish-audioset-1.pb https://essentia.upf.edu/models/classifiers/mood_happy/mood_happy-vggish-audioset-1.pb
+
+     curl -s -o models/mood_party-musicnn-msd-2.pb https://essentia.upf.edu/models/classifiers/mood_party/mood_party-musicnn-msd-2.pb
+     curl -s -o models/mood_party-musicnn-mtt-2.pb https://essentia.upf.edu/models/classifiers/mood_party/mood_party-musicnn-mtt-2.pb
+     curl -s -o models/mood_party-vggish-audioset-1.pb https://essentia.upf.edu/models/classifiers/mood_party/mood_party-vggish-audioset-1.pb
+
+     curl -s -o models/mood_relaxed-musicnn-msd-2.pb https://essentia.upf.edu/models/classifiers/mood_relaxed/mood_relaxed-musicnn-msd-2.pb
+     curl -s -o models/mood_relaxed-musicnn-mtt-2.pb https://essentia.upf.edu/models/classifiers/mood_relaxed/mood_relaxed-musicnn-mtt-2.pb
+     curl -s -o models/mood_relaxed-vggish-audioset-1.pb https://essentia.upf.edu/models/classifiers/mood_relaxed/mood_relaxed-vggish-audioset-1.pb
+
+     curl -s -o models/mood_sad-musicnn-msd-2.pb https://essentia.upf.edu/models/classifiers/mood_sad/mood_sad-musicnn-msd-2.pb
+     curl -s -o models/mood_sad-musicnn-mtt-2.pb https://essentia.upf.edu/models/classifiers/mood_sad/mood_sad-musicnn-mtt-2.pb
+     curl -s -o models/mood_sad-vggish-audioset-1.pb https://essentia.upf.edu/models/classifiers/mood_sad/mood_sad-vggish-audioset-1.pb
