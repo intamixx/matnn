9e9dfcd6079434c63ffb834e7402ff00b0504858
Intamixx <intamixx@users.noreply.github.com>
2024-01-05 13:51:42 +0000

    Update approachability-engagement.py

--- musicnn/approach-engage/approachability-engagement.py
+++ musicnn/approach-engage/approachability-engagement.py
@@ -37,7 +37,7 @@ def predict(audio_file):
 
     #model = str(MODELS_HOME / "discogs-effnet-bs64-1.pb")
     model = str("discogs-effnet-bs64-1.pb")
-    print (model)
+    #print (model)
     input = "model/Placeholder"
     output = "model/Softmax"
     sample_rate = 16000
@@ -55,16 +55,16 @@ def predict(audio_file):
     # get model_types from dict keys
     global model_types
     model_types = models.keys()
-    print(f"model_types: {model_types}")
+    #print(f"model_types: {model_types}")
     # load all models in classifiers for all model_types
     all_models = [models[model_type][key] for model_type in model_types for key in models[model_type].keys()]
     # build a dict of dicts to handle classifiers for each model type
     for model in all_models:
-        print ("---------------")
-        print (model)
+        #print ("---------------")
+        #print (model)
         #modelFilename = str(MODELS_HOME / f"{model['name']}.pb")
         modelFilename = str(f"{model['name']}.pb")
-        print (modelFilename)
+        #print (modelFilename)
         classifiers[model["name"]] = TensorflowPredict2D(
             graphFilename=modelFilename,
             input=input,
@@ -72,37 +72,41 @@ def predict(audio_file):
         )
 
 
-    print("loading audio...")
+    #print("loading audio...")
     loader.configure(sampleRate=sample_rate, filename=str(audio_file), resampleQuality=4)
     waveform = loader()
 
-    print ("MODEL TYPE ;;;;;;;;;;;;;;;;;;;")
+    #print ("MODEL TYPE ;;;;;;;;;;;;;;;;;;;")
     #model_type = "regression"
     #model_type = "2 classes"
     model_type = "3 classes"
-    print (model_type)
+    #print (model_type)
     table = _run_models(waveform, model_type, audio_file)
 
-    out_path = Path(tempfile.mkdtemp()) / "out.md"
-    with open(out_path, "w") as f:
-        f.write(table)
+    #out_path = Path(tempfile.mkdtemp()) / "out.md"
+    #with open(out_path, "w") as f:
+    #    f.write(table)
 
-    print("done!")
-    return out_path
+    #print("done!")
+    #return out_path
+    return
 
 def _run_models(waveform: np.ndarray, model_type: str, title: str):
     embeddings = tensorflowPredictEffnetDiscogs(waveform)
 
-
     # the header and bar table should change for a regression model
     table = initialize_table(model_type, title)
+    model_list = []
+    ae = {}
 
     # define a list of models for the model_type
+    #for key in models[model_type].keys():
+    #    print (key)
+    #    print (models[model_type][key])
     model_list = [models[model_type][key] for key in models[model_type].keys()]
-    print ("ooooooooooooooooooooooooooooooooo")
-    print (model_list)
+    #print (model_list)
 
-    print("running classification heads...")
+    #print("running classification heads...")
     if model_type == "regression":
         # predict with each regression model
         for model in model_list:
@@ -128,8 +132,12 @@ def _run_models(waveform: np.ndarray, model_type: str, title: str):
             top_class = np.argmax(average)
             for i, label in enumerate(model["labels"]):
                 labels.append(label)
+                #print (label)
+                #print (f"{average[i]:.2f}")
                 if i == top_class:
                     activations.append(f"**{average[i]:.2f}**")
+                    #print (f"{model['friendly_name']}: {label}")
+                    ae[model['friendly_name']]=label
                 else:
                     activations.append(f"{average[i]:.2f}")
 
@@ -139,6 +147,7 @@ def _run_models(waveform: np.ndarray, model_type: str, title: str):
             table += f"{model['name']} | {labels} | {activations}\n"
             if model != model_list[-1]:
                 table += "||||\n"  # separator for readability
+        print (ae)
     return table
 
 def main(argv):
@@ -158,10 +167,10 @@ def main(argv):
         print(('%s -f <file>') % format(os.path.abspath(__file__)))
         sys.exit(2)
     result = predict(file)
-    print (result)
-    if not result:
-        print ("No suitable genre found")
-        sys.exit(2)
+    #print (result)
+    #if not result:
+    #    print ("No suitable genre found")
+    #    sys.exit(2)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
