758cdcba2c782e13967fd91163403da48e3463b4
Intamixx <intamixx@users.noreply.github.com>
2024-06-03 18:42:10 +0100

    Create App.vue

---
+++ vue-app/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bg-secondary">
+    <Header />
+    <router-view></router-view>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Footer,
+  },
+};
+</script>
