ddca0a9626f133052521ef3a8f1d2c1e8c9f16e1
Intamixx <intamixx@users.noreply.github.com>
2024-06-03 18:46:34 +0100

    Create Header.vue

---
+++ vue-app/src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <div class="navbar-brand" href="#">Matnn (<u>M</u>usic <u>A</u>udio <u>T</u>agger <u>N</u>eural <u>N</u>et)</div>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link class="nav-link active" aria-current="page" to="/"
+              >Home</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link active" aria-current="page" to="/status"
+              >Status</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link active" aria-current="page" to="/result"
+              >Result</router-link
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
