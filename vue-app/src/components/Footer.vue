cb0856005e63859dbf3867fda47b5321bdfeb4af
Intamixx <intamixx@users.noreply.github.com>
2024-06-03 18:45:17 +0100

    Create Footer.vue

---
+++ vue-app/src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer class="footer py-3 bg-dark" style="position: relative; bottom:0px; left: 0px; width: 100%;">
+    <div class="container">
+      <span class="text-muted">Copyrighted by Intamixx</span>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style scoped>
+/*  footer {
+  width: 100%;
+  height: 55px;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+} */
+</style>
