a94d8f07115a0ee0750bb551692684d00bf8bfad
Intamixx <intamixx@users.noreply.github.com>
2024-06-03 18:44:10 +0100

    Create index.js

---
+++ vue-app/src/router/index.js
@@ -0,0 +1,34 @@
+import { createWebHistory, createRouter } from 'vue-router'
+import Home from '../components/Home'
+import Status from "../components/Status"
+import Result from "../components/Result"
+
+//import VueRouter from 'vue-router';
+
+const routes = [
+    {
+        path: "/",
+        component: Home,
+    },
+    {
+        name: "Status",
+        path: "/status/:id",
+        component: Status,
+        //component: () => import('../components/Status.vue'),
+        //props: true
+    },
+    {
+        name: "Result",
+        path: "/result/:id",
+        component: Result,
+        //component: () => import('../components/Result.vue'),
+        //props: true
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
