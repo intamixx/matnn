65b469014a9e830f98403114622927d439154977
Intamixx <intamixx@users.noreply.github.com>
2025-02-25 11:44:51 +0000

    Update main.tf

--- terraform/kueue/main.tf
+++ terraform/kueue/main.tf
@@ -1,5 +1,5 @@
 # Configure Terraform for RKE / Kubernetes / Kueue
-# (workload schema errors during installation -> kubectl apply --server-side -f https://github.com/kubernetes-sigs/kueue/releases/download/v0.6.2/manifests.yaml --force-conflicts)
+# (workload schema errors during installation -> kubectl apply --server-side -f https://github.com/kubernetes-sigs/kueue/releases/download/v0.10.1/manifests.yaml --force-conflicts)
 terraform {
   required_providers {
     azuread = {
@@ -14,28 +14,55 @@ terraform {
 }
 
 resource "rke_cluster" "cluster" {
-  nodes {
-    address = "137.117.234.92"
-    internal_address = "10.123.1.5"
-    user    = "root"
-    role    = ["controlplane", "worker", "etcd"]
-    ssh_key = "${file("/tmp/rootprivkey")}"
-  } 
-  nodes {
-    address = "137.117.234.108"
-    internal_address = "10.123.1.4"
-    user    = "root"
-    role    = ["worker", "etcd"]
-    ssh_key = "${file("/tmp/rootprivkey")}"
+  # Other configurations for RKE cluster
+
+  # Using dynamic block to loop through nodes
+  dynamic "nodes" {
+    for_each = var.nodes
+
+    content {
+      address          = nodes.value.address
+      internal_address = nodes.value.internal_address
+      user             = nodes.value.user
+      role             = nodes.value.role
+      ssh_key          = file(nodes.value.ssh_key)
+    }
   }
   enable_cri_dockerd = true
     addons_include = [
-    "https://github.com/kubernetes-sigs/kueue/releases/download/v0.6.2/manifests.yaml",
-    "https://raw.githubusercontent.com/kubernetes-sigs/kueue/main/site/static/examples/admin/single-clusterqueue-setup.yaml"
+    "https://github.com/kubernetes-sigs/kueue/releases/download/v0.10.1/manifests.yaml",
+    "https://raw.githubusercontent.com/kubernetes-sigs/kueue/main/site/static/examples/admin/single-clusterqueue-setup.yaml",
+    #"https://github.com/kubernetes-sigs/kueue/releases/download/v0.10.1/prometheus.yaml"
     ]
 }
 
 resource "local_file" "kube_cluster_yaml" {
   filename = "${path.root}/kube_config_cluster.yml"
   sensitive_content  = "${rke_cluster.cluster.kube_config_yaml}"
+
+  # Establishes connection to be used by all
+  # generic remote provisioners (i.e. file/remote-exec)
+  connection {
+    type     = "ssh"
+    user     = "root"
+    #password = var.root_password
+    #private_key = file("${path.module}/hello.txt")
+    private_key = file("~/.ssh/id_rsa")
+    host     = "40.118.87.219"
+  }
+
+  provisioner "file" {
+    source      = "kube_config_cluster.yml"
+    destination = "/root/.kube/config"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "kubectl apply --server-side -f https://github.com/kubernetes-sigs/kueue/releases/download/v0.10.1/manifests.yaml --force-conflicts",
+      "sleep 10",
+      "kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/kueue/main/site/static/examples/admin/single-clusterqueue-setup.yaml",
+      "sleep 5",
+      #"kubectl apply --server-side -f https://github.com/kubernetes-sigs/kueue/releases/download/v0.10.1/prometheus.yaml"
+    ]
+  }
 }
