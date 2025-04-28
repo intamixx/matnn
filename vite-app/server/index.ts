efb1de04605f4b963bee883c0e9529f5ca154600
Intamixx <intamixx@users.noreply.github.com>
2025-04-28 13:30:30 +0100

    Update index.ts

--- vite-app/server/index.ts
+++ vite-app/server/index.ts
@@ -59,7 +59,7 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = 5000;
+  const port = 9090;
   server.listen({
     port,
     host: "0.0.0.0",
