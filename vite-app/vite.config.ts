7e1982e14877fec6ad88bae9ac5ee00945269447
Intamixx <intamixx@users.noreply.github.com>
2025-05-01 12:36:31 +0100

    Update vite.config.ts

--- vite-app/vite.config.ts
+++ vite-app/vite.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: path.resolve(import.meta.dirname, "dist"),
     emptyOutDir: true,
   },
   server: {
