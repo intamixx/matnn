773fd42e840ce4c7fd03f131c18df746b74ff79b
Intamixx <intamixx@users.noreply.github.com>
2025-06-03 13:26:20 +0100

    Update main.tf

--- terraform/terraform-rke2/main.tf
+++ terraform/terraform-rke2/main.tf
@@ -8,26 +8,6 @@ terraform {
   }
 }
 
-variable "nodes" {
-  type = map(object({
-    address          = string
-    internal_address = string
-    user             = string
-    ssh_key          = string
-    role             = list(string)
-  }))
-}
-
-variable "cni" {
-  type    = string
-  default = "cilium"
-  description = "CNI plugin to install. Supported: cilium, calico, none"
-  validation {
-    condition     = contains(["cilium", "calico", "none"], var.cni)
-    error_message = "cni must be one of cilium, calico, or none"
-  }
-}
-
 locals {
   join_info = {
     url   = try(module.rke2_nodes["node1"].join_url, "")
