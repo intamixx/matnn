c8123c97ce38bcb23c9dfc035b1056bcd7b86259
Intamixx <intamixx@users.noreply.github.com>
2025-06-03 13:26:36 +0100

    Update variables.tf

--- terraform/terraform-rke2/variables.tf
+++ terraform/terraform-rke2/variables.tf
@@ -1,9 +1,19 @@
 variable "nodes" {
   type = map(object({
-    address           = string
-    internal_address  = string
-    user              = string
-    role              = list(string)
-    ssh_key           = string
+    address          = string
+    internal_address = string
+    user             = string
+    ssh_key          = string
+    role             = list(string)
   }))
+}
+
+variable "cni" {
+  type    = string
+  default = "cilium"
+  description = "CNI plugin to install. Supported: cilium, calico, none"
+  validation {
+    condition     = contains(["cilium", "calico", "none"], var.cni)
+    error_message = "cni must be one of cilium, calico, or none"
+  }
 }
