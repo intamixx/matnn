3b4134e019c846738c88a76a8e9b29c8e159efa3
Intamixx <intamixx@users.noreply.github.com>
2025-07-25 20:52:06 +0100

    Create main.tf

---
+++ terraform/terraform-k8s/main.tf
@@ -0,0 +1,127 @@
+provider "null" {}
+
+locals {
+  controlplane_nodes = {
+    for k, v in var.nodes : k => v if contains(v.role, "control-plane")
+  }
+  worker_nodes = {
+    for k, v in var.nodes : k => v if contains(v.role, "worker")
+  }
+  server_node_key = length(keys(local.controlplane_nodes)) > 0 ? keys(local.controlplane_nodes)[0] : ""
+  server_node = local.controlplane_nodes[local.server_node_key]
+
+  pod_cidr = "10.10.0.0/16"
+}
+
+# Provision the bootstrap control-plane node
+resource "null_resource" "bootstrap_control_plane" {
+  connection {
+    type        = "ssh"
+    user        = local.server_node.user
+    private_key = file(local.server_node.ssh_key)
+    host        = local.server_node.address
+  }
+
+  provisioner "file" {
+    source      = "cloudinit_master.sh"
+    destination = "/tmp/cloudinit_master.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/cloudinit_master.sh",
+      "/tmp/cloudinit_master.sh ${local.pod_cidr}"
+    ]
+  }
+}
+
+# Provision all worker nodes
+resource "null_resource" "worker_nodes" {
+  for_each = local.worker_nodes
+
+  connection {
+    type        = "ssh"
+    user        = each.value.user
+    private_key = file(each.value.ssh_key)
+    host        = each.value.address
+  }
+
+  provisioner "file" {
+    source      = "cloudinit_worker.sh"
+    destination = "/tmp/cloudinit_worker.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/cloudinit_worker.sh",
+      "/tmp/cloudinit_worker.sh ${local.server_node.address}"
+    ]
+  }
+
+  depends_on = [null_resource.bootstrap_control_plane]
+}
+
+resource "null_resource" "label_nodes" {
+  depends_on = [null_resource.worker_nodes]
+
+  connection {
+    host        = local.server_node.address
+    user        = local.server_node.user
+    private_key = file(local.server_node.ssh_key)
+  }
+
+  provisioner "remote-exec" {
+    inline = concat(
+      ["sleep 20"],
+      flatten([
+        for node_name, node_data in var.nodes : [
+          for role in node_data.role :
+            "sudo kubectl label node ${node_name} node-role.kubernetes.io/${role}='true' --overwrite"
+        ]
+      ])
+    )
+  }
+}
+
+resource "null_resource" "fetch_kubeconfig" {
+  depends_on = [null_resource.rke2_server_install]
+
+  provisioner "remote-exec" {
+    connection {
+      host        = local.server_node.address
+      user        = local.server_node.user
+      private_key = file(local.server_node.ssh_key)
+    }
+
+    inline = [
+      "cat /etc/kubernetes/admin.conf"
+    ]
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+      ssh -o StrictHostKeyChecking=no -i ${local.server_node.ssh_key} ${local.server_node.user}@${local.server_node.address} \
+      "sudo cat /etc/kubernetes/admin.conf" | sed 's|127.0.0.1|${local.server_node.address}|' > ./kube_config_cluster.yaml
+    EOT
+  }
+}
+
+data "local_file" "kubeconfig" {
+  depends_on = [null_resource.fetch_kubeconfig]
+  filename   = "${path.module}/kube_config_cluster.yaml"
+}
+
+locals {
+  kubeconfig = yamldecode(data.local_file.kubeconfig.content)
+  ca_cert    = local.kubeconfig.clusters[0].cluster["certificate-authority-data"]
+}
+
+output "kube_config_file" {
+  value     = data.local_file.kubeconfig.content
+  sensitive = true
+}
+
+output "ca_certificate" {
+  value     = local.ca_cert
+  sensitive = true
+}
