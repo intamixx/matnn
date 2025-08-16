a4da734f5ce86ab1869d9018c333530f0a636cb6
Intamixx <intamixx@users.noreply.github.com>
2025-08-16 16:17:39 +0100

    Update cloudinit_master.sh

--- terraform/terraform-k8s/cloudinit_master.sh
+++ terraform/terraform-k8s/cloudinit_master.sh
@@ -13,8 +13,12 @@ apt-mark hold kubelet kubeadm kubectl
 sudo systemctl restart kubelet.service
 sudo systemctl enable --now kubelet
 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - &&
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" &&
+sudo apt-get update -y &&
+sudo apt-get install containerd.io -y
+
 # Install containerd
-apt-get install -y containerd.io
 mkdir -p /etc/containerd
 containerd config default | tee /etc/containerd/config.toml
 systemctl restart containerd
@@ -36,11 +40,12 @@ VERSION="v1.30.0"  # change to latest version if needed
 curl -LO https://github.com/kubernetes-sigs/cri-tools/releases/download/$VERSION/crictl-$VERSION-linux-amd64.tar.gz
 sudo tar -C /usr/local/bin -xzvf crictl-$VERSION-linux-amd64.tar.gz
 
+
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
 
-sudo mkdir /root/.kube
+sudo mkdir -p /root/.kube
 
 ssh-keygen -t rsa -f ~/.ssh/id_rsa -N "" <<< y
 
@@ -48,10 +53,9 @@ sudo cat > /root/.ssh/authorized_keys <<EOF
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDIVKQIn052nShq9YIxASQyvumGJS9hjc2QXJm5igzlcD94a+5YdP1eA0vjx3cRaawaenJm4VW68s2hp2SQEte2zS9uxAJ9ZnY6lLnYGr1uPQYNt7raEl0A596hQQ0fpKOyBph9cLUkasyfczTJQ/NptgYZisidx7EuTr32tLOXyrxWviav4HzFuLQTqMmRgqae20TNvRtKN33a+SjbJIAPlpkMGr2H11Gv73lmakxFY+zMV/SfquIYYID3yxeW9TdA+feSwbp1JjDppKRj9tuHa9vGTx4zgM8o6g29BgeWn4Ee5pS/zsX+l7Er3vX8vjjveaeNw4xJ2akY+C3jev2q2IxI83MuCppG1JjLhFBVkLCBuN+9svRYiGVDIZYeL4f67Uf6Recrd78lMbPxp76i3T1UGfMjEjjvWO/mz7Pp10FvT2g23mg+N41PWhU7jmyAtLPUEFL5UXpQEve8gcwpwnQ+IEbkghNbit3TAOwPuFLCmzihGNnC4x6YCT8Yrek= adm-ku66289@kuhpcgw04.kuhpc.local
 EOF
 
-
 # Init Kubernetes
 POD_CIDR=$1
-kubeadm init --pod-network-cidr=${POD_CIDR} --upload-certs > /root/kubeinit.log
+kubeadm init --pod-network-cidr=${POD_CIDR} --upload-certs
 
 # Setup kubeconfig for root
 mkdir -p /root/.kube
@@ -62,7 +66,10 @@ kubeadm token create --print-join-command > /root/join.sh
 chmod +x /root/join.sh
 
 # Apply Calico
-kubectl --kubeconfig=/root/.kube/config apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.30.0/manifests/tigera-operator.yaml
-wget https://raw.githubusercontent.com/projectcalico/calico/v3.30.0/manifests/custom-resources.yaml
-sed -i 's/cidr: 192\.168\.0\.0\/16/cidr: ${POD_CIDR}/g' custom-resources.yaml
+sleep 30
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.1/manifests/tigera-operator.yaml
+sleep 10
+curl https://raw.githubusercontent.com/projectcalico/calico/v3.28.1/manifests/custom-resources.yaml -O
+sed -i 's/cidr: 192\.168\.0\.0\/16/cidr: 10.10.0.0\/16/g' custom-resources.yaml
+sleep 5
 kubectl create -f custom-resources.yaml
